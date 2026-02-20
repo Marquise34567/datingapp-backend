504b30390274e0ef18af5f4f00101b25f21ceafb
Auto-Editor User <user@auto-editor.local>
2026-02-20 01:44:44 -0500

    frontend: add premium maintenance overlay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import PremiumDatingAdvicePage from "./components/PremiumDatingAdvicePage";
+import Maintenance from "./components/Maintenance";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PremiumDatingAdvicePage />
+    <Maintenance />
   </React.StrictMode>
 );
  
